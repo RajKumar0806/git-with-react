fc72941344615bb474b9ac74cf78ed611dca338c
Rajkumar0806 <rajjadhav219@gmail.com>
2024-08-14 15:59:06 +0530

    created new component Header and Home

---
+++ src/component/header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+
+<h1>Welcome to Git-With-React</h1>
+
+
+    </div>
+
+  )
+}
+
+export default Header;
