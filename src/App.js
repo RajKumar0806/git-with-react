2e52029fb24f34463a7d755ebe0f77946e019549
rampipalwa197 <rampipalwa123@gmail.com>
2024-08-14 16:22:44 +0530

    about component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import About from './component/about/About';
 import Header from './component/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <About/>
     </div>
   );
 }
